ea4385d53d509ca25e7578a3146570c129f81ad0
Stefanus Titan Elianto <107969099+StefanusTitan@users.noreply.github.com>
2025-08-24 23:04:25 +0700

    refactor: add ESLint configuration to ignore errors during production builds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  // Add ESLint build option from the docs. This lets production builds
+  // complete even if ESLint reports errors during build.
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["your-image-domain.com"], // Replace with your image domain if needed
   },
